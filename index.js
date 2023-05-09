e74e4f292fe87e3967c79822b010f347daf65893
miguel heckler lencina <miguellencina13@gmail.com>
2023-05-08 23:06:24 -0300

    Added new async function extract_async

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const fs = require("fs");
+const fsPromise = require("fs/promises");
+const { Writable } = require("stream");
+const util = require("util");
 const MAGIC_POSIX = Buffer.from("ustar\x0000", "ascii");
 const MAGIC_GNU = Buffer.from("ustar\040\040\0");
 
+const fs_open_async = util.promisify(fs.open);
+const fs_close_async = util.promisify(fs.close);
+const fs_read_async = util.promisify(fs.read);
+const fs_write_async = util.promisify(fs.write);
 function nextSector(tarball_fd){
     const buf = Buffer.allocUnsafe(512);
     fs.readSync(tarball_fd, buf);
+    return buf;
+}
+async function async_nextSector(tarball_fd){
+    const buf = Buffer.allocUnsafe(512);
+    await fs_read_async(tarball_fd, buf);
     return buf;
 }
 function compare(buf1, buf2){
@@ -25,9 +37,7 @@ function checkMagic(sectorData){
 function toOctal(buf){
     return parseInt(buf.toString("ascii"), 8);
 }
-function nextHeader(tarball_fd){
-    const sector = nextSector(tarball_fd);
-    
+function parseHeader(sector){
     const name = sector.subarray(0,100).toString("ascii").replace(/\0/g, "");
     const mode = toOctal(sector.subarray(100,108));
     const owneruid = toOctal(sector.subarray(108,116));
@@ -68,18 +78,27 @@ function nextHeader(tarball_fd){
     };
     return obj;
 }
-function log(str, shouldLog){
-    if(shouldLog){
-        console.log(str);
-    }
+function nextHeader(tarball_fd){
+    const sector = nextSector(tarball_fd);
+    return parseHeader(sector);
+}
+async function async_nextHeader(tarball_fd){
+    const sector = await async_nextSector(tarball_fd);
+    return parseHeader(sector);
 }
 /**
- * Extract a tar (synchronous, so it can stops the event handler)
- * @param {fs.PathLike} tarballPath 
- * @param {boolean | false} shouldLogToConsole 
- * @returns 
+ * @param {string} str 
+ * @param {Writable | undefined} log_stream 
  */
-function extract(tarballPath, shouldLogToConsole = false){
+function log(str, log_stream){
+    log_stream?.write(str + "\n");
+}
+/**
+ * Extracts a tar (synchronous, so it stops the event handler)
+ * @param {fs.PathLike} tarballPath A path location to the tarball.
+ * @param {Writable | undefined} logStream A log stream. Useful on applications that uses log files
+ */
+function extract(tarballPath, logStream){
     if(!fs.existsSync(tarballPath)){
         throw new Error("Non-existant path");
     }
@@ -90,15 +109,17 @@ function extract(tarballPath, shouldLogToConsole = false){
         if((!checkMagic(header._originalSector))){
             break;
         }
-        log(header._realFileName, shouldLogToConsole);
+        log(`> ${header._realFileName}`, logStream);
         
         if(header.typeFlag === "5"){
+            log(`- is a directory.`, logStream);
             fs.mkdirSync(header._realFileName, {recursive: true});
             continue;
         }
 
         if(header.typeFlag === "\x00" || header.typeFlag === "0"){
             let size = header.size;
+            log(`- is a file. Need to read ${Math.ceil(size / 512)} sectors.`, logStream);
             let sectors = 0;
             const fd = fs.openSync(header._realFileName, "w");
             while(true){
@@ -113,9 +134,61 @@ function extract(tarballPath, shouldLogToConsole = false){
                 size -= 512;
             }
             fs.closeSync(fd);
+            log(`- done.`, logStream);
             continue;
         }
     }
     fs.closeSync(tar_fd);
 }
-module.exports = extract;
+async function exists_promise(path){ // https://stackoverflow.com/a/35008327
+    return fsPromise.access(path, fs.constants.F_OK).then(() => true).catch(() => false);
+}
+/**
+ * Extracts a tar (asynchronous, so it does not stops the event handler)
+ * @param {fs.PathLike} tarballPath A path location to the tarball.
+ * @param {Writable | undefined} logStream A log stream. Useful on applications that uses log files
+ * @returns {Promise<void>}
+ */
+async function extract_async(tarballPath, logStream){
+    if(!await exists_promise(tarballPath)){
+        throw new Error("Non-existant path");
+    }
+    const tar_fd = await fs_open_async(tarballPath, "r");
+
+    while(true){
+        const header = await async_nextHeader(tar_fd);
+        if((!checkMagic(header._originalSector))){
+            break;
+        }
+        log(`> ${header._realFileName}`, logStream);
+        
+        if(header.typeFlag === "5"){
+            log(`- is a directory.`, logStream);
+            await fsPromise.mkdir(header._realFileName, {recursive: true});
+            continue;
+        }
+
+        if(header.typeFlag === "\x00" || header.typeFlag === "0"){
+            let size = header.size;
+            log(`- is a file. Need to read ${Math.ceil(size / 512)} sectors.`, logStream);
+            let sectors = 0;
+            const fd = await fs_open_async(header._realFileName, "w");
+            while(true){
+                const sector = await async_nextSector(tar_fd);
+                sectors++;
+                if(size < 512){
+                    const croppedData = sector.subarray(0, size);
+                    await fs_write_async(fd, croppedData);
+                    break;
+                }
+                await fs_write_async(fd, sector);
+                size -= 512;
+            }
+            await fs_close_async(fd);
+            log(`- done.`, logStream);
+            continue;
+        }
+    }
+    await fs_close_async(tar_fd);
+}
+module.exports = {extract, extract_async};
--- test.js
+++ test.js
@@ -1,2 +1,32 @@
 const extractTar = require(".");
-extractTar("./test.tar");
+const fs = require("fs");
+const stream = require("stream");
+const logStream = new stream.Writable({
+    write(chunk, encoding, callback) {
+        process.stdout.write(`[SYNC] ${chunk}`);
+        callback();
+      }
+});
+const logStreamAsync = new stream.Writable({
+    write(chunk, encoding, callback) {
+        process.stdout.write(`[ASYNC] ${chunk}`);
+        callback();
+      }
+});
+try{
+    fs.rmSync("./test", {recursive: true});
+}catch{}
+try{
+    extractTar.extract("./test.tar", logStream);
+    console.log("[1] Passed\n");
+}catch{
+    console.error("[1] Not passed!");
+    process.exit(1);
+}
+extractTar.extract_async("./test.tar", logStreamAsync).then(() => {
+    console.log("[2] Passed\n");
+    console.log("All tests done!");
+}).catch(() => {
+    console.error("[2] Not passed!");
+    process.exit(1);
+});
